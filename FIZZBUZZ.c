2a00ac8bd42ccccf5705cc837408ec5babf24a5b
extrump <44114459+extrump@users.noreply.github.com>
2019-02-11 18:32:58 +0100

    Add files via upload

---
+++ FIZZBUZZ.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define BUZZZ int
+#define FIZZZ printf
+#define FIZZ for
+#define BUZZ if
+#define BZZ "\n"
+#define FUZZ return
+#define FIZZBUZZZ main
+#define FIZZZBUZZ "%d"
+#define FIZZBUZZ else
+#define BUZZFIZZ 3
+#define FIZZFIZZ 5
+#define BUZZBUZZ 100
+
+BUZZZ FIZZBUZZZ()
+
+	{
+
+	BUZZZ fizzbuzz;
+	FIZZ(fizzbuzz=BUZZFIZZ/BUZZFIZZ; fizzbuzz<=BUZZBUZZ; ++fizzbuzz)
+
+		{
+		BUZZ(fizzbuzz%(BUZZFIZZ*FIZZFIZZ)==BUZZBUZZ-BUZZBUZZ)FIZZZ("FIZZBUZZ");
+		FIZZBUZZ BUZZ(fizzbuzz%BUZZFIZZ==FIZZFIZZ-FIZZFIZZ)FIZZZ("FIZZ");
+		FIZZBUZZ BUZZ(fizzbuzz%FIZZFIZZ==BUZZFIZZ-BUZZFIZZ)FIZZZ("BUZZ");
+		FIZZBUZZ FIZZZ(FIZZZBUZZ,fizzbuzz);
+		FIZZZ(BZZ);
+		}
+
+	FUZZ BUZZBUZZ-fizzbuzz;
+
+	}
